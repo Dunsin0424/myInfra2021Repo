ee131ebb5daaf588bae6d4358458cfa3fd255f31
Dunsin0424 <129049390+Dunsin0424@users.noreply.github.com>
2023-08-03 14:15:56 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "mytf-state-bucket"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "dynamo-db-table"
   }
 }
